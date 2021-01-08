fc7c1866f8a820ef0de14c418fe3fa3dc5b687bd
amtksms97 <amtksms97@gmail.com>
2021-01-08 18:08:52 +0530

    Added code to get and set the values

--- address_book.py
+++ address_book.py
@@ -9,23 +9,41 @@ class Person:
         self.zip_code = zip_code
         self.phone = phone
 
-    def first_name(self):
+    def get_first_name(self):
         return self.first_name
 
-    def second_name(self):
+    def get_second_name(self):
         return self.second_name
 
-    def email_address(self):
+    def get_email_address(self):
         return self.email_address
 
-    def state(self):
+    def get_state(self):
         return self.state
 
-    def zip(self):
+    def get_zip(self):
         return self.zip_code
 
-    def phone(self):
+    def get_phone(self):
         return self.phone
+
+    def set_first_name(self, first_name):
+        self.first_name = first_name
+
+    def set_second_name(self, second_name):
+        self.second_name = second_name
+
+    def set_email_address(self, email_address):
+        self.email_address = email_address
+
+    def set_state(self, state):
+        self.state = state
+
+    def set_zip(self, zip_code):
+        self.zip_code = zip
+
+    def set_phone(self, phone):
+        self.phone = phone
 
 
 print("Welcome to Address Book Program")
